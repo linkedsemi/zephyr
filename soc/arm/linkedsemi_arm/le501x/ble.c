015252e8afddb57ee59ccdee10789cbf604bf090
Xintian Zhang <xtzhang@linkedsemi.com>
2023-01-31 11:01:55 +0800

    update

---
+++ soc/arm/linkedsemi_arm/le501x/ble.c
@@ -0,0 +1,210 @@
+#include <zephyr/kernel.h>
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/drivers/bluetooth/hci_driver.h>
+#define LOG_LEVEL CONFIG_BT_HCI_DRIVER_LOG_LEVEL
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(ble_hci);
+
+#define LL_THREAD_STACK_SIZE  0x200
+#define LL_THREAD_PRIORITY 0
+
+#define HCI_CMD                 0x01
+#define HCI_ACL                 0x02
+#define HCI_SCO                 0x03
+#define HCI_EVT                 0x04
+#define HCI_ISO                 0x05
+
+static K_SEM_DEFINE(hci_send_sem, 0, 1);
+static K_SEM_DEFINE(ll_thread_sem, 0, 1);
+K_THREAD_STACK_DEFINE(ll_thread_stack, LL_THREAD_STACK_SIZE);
+struct k_thread ll_thread_data;
+
+void (*ll_hci_write_callback)(void *,uint8_t);
+void *ll_hci_write_param;
+void (*ll_hci_read_callback)(void *,uint8_t);
+void *ll_hci_read_param;
+uint8_t *ll_hci_read_buf;
+uint16_t ll_hci_read_size;
+
+struct net_buf *host_send_buf;
+uint8_t *host_send_buf_current_ptr;
+uint16_t host_send_buf_valid_len;
+bool ll_hci_done_read;
+
+void ble_ll_task_event_set()
+{
+    k_sem_give(&ll_thread_sem);
+}
+
+void aos_swint_set();
+static void hci_copy_from_send_buf()
+{
+    if(ll_hci_read_size == 0) return;
+    if(host_send_buf_valid_len == 0) 
+    {
+        if(host_send_buf)
+        {
+            net_buf_unref(host_send_buf);
+        }
+        k_sem_give(&hci_send_sem);
+        return;
+    }
+    memcpy(ll_hci_read_buf,host_send_buf_current_ptr,ll_hci_read_size);
+    host_send_buf_current_ptr += ll_hci_read_size;
+    host_send_buf_valid_len -= ll_hci_read_size;
+    ll_hci_read_size = 0;
+    ll_hci_done_read = true;
+    aos_swint_set();
+}
+
+void ll_hci_read(uint8_t *bufptr, uint32_t size, void (*callback)(void *,uint8_t), void* dummy)
+{
+    ll_hci_read_buf = bufptr;
+    ll_hci_read_size = size;
+    ll_hci_read_callback = callback;
+    ll_hci_read_param = dummy;
+    hci_copy_from_send_buf();
+}
+
+void ll_hci_write(uint8_t *bufptr, uint32_t size, void (*callback)(void *,uint8_t), void* dummy)
+{
+    ll_hci_write_callback = callback;
+    ll_hci_write_param = dummy;
+	uint8_t pkt_indicator = bufptr[0];
+    bufptr += 1;
+    size -= 1;
+    struct net_buf *buf = NULL;
+    switch(pkt_indicator)
+    {
+    case HCI_EVT:
+    {
+	    struct bt_hci_evt_hdr hdr;
+        memcpy((void *)&hdr, bufptr, sizeof(hdr));
+        bufptr += sizeof(hdr);
+        size -= sizeof(hdr);
+        __ASSERT_NO_MSG(hdr.len == size);
+        buf = bt_buf_get_evt(hdr.evt,false,K_NO_WAIT);
+        __ASSERT_NO_MSG(buf);
+        net_buf_add_mem(buf,&hdr,sizeof(hdr));
+    }
+    break;
+    case HCI_ACL:
+    {
+        struct bt_hci_acl_hdr hdr;
+		memcpy((void *)&hdr, bufptr, sizeof(hdr));
+		bufptr += sizeof(hdr);
+		size -= sizeof(hdr);
+        __ASSERT_NO_MSG(hdr.len == size);
+	    buf = bt_buf_get_rx(BT_BUF_ACL_IN, K_NO_WAIT);
+        __ASSERT_NO_MSG(buf);
+		net_buf_add_mem(buf, &hdr, sizeof(hdr));
+    }
+    break;
+    default:
+    	LOG_ERR("Unknown HCI type %u", pkt_indicator);
+        while(1);
+    }
+    size_t buf_tailroom = net_buf_tailroom(buf);
+    (void)(buf_tailroom);
+    __ASSERT_NO_MSG(buf_tailroom>=size);
+    net_buf_add_mem(buf,bufptr,size);
+    bt_recv(buf);
+    ll_hci_done_read = false;
+    aos_swint_set();
+}
+
+void ll_hci_flow_on()
+{
+
+}
+
+bool ll_hci_flow_off()
+{
+    return true;
+}
+
+static int hci_driver_send(struct net_buf *buf)
+{
+    uint8_t pkt_indicator;
+	LOG_DBG("buf %p type %u len %u", buf, bt_buf_get_type(buf), buf->len);
+	switch (bt_buf_get_type(buf)) {
+	case BT_BUF_ACL_OUT:
+		pkt_indicator = HCI_ACL;
+		break;
+	case BT_BUF_CMD:
+		pkt_indicator = HCI_CMD;
+		break;
+	case BT_BUF_ISO_OUT:
+		pkt_indicator = HCI_ISO;
+		break;
+	default:
+		LOG_ERR("Unknown type %u", bt_buf_get_type(buf));
+		while(1);
+	}
+    net_buf_push_u8(buf,pkt_indicator);
+	LOG_HEXDUMP_DBG(buf->data, buf->len, "Final HCI buffer:");
+	if (k_sem_take(&hci_send_sem, K_FOREVER) == 0) {
+        host_send_buf = buf;
+        host_send_buf_current_ptr = buf->data;
+        host_send_buf_valid_len = buf->len;
+        hci_copy_from_send_buf();
+	} else {
+		LOG_ERR("Send packet timeout error");
+		while(1);
+	}
+    return 0;
+}
+
+static int hci_driver_open()
+{
+    return 0;
+}
+
+static int hci_driver_close()
+{
+    return 0;
+}
+
+static const struct bt_hci_driver drv = {
+	.name	= "ble_hci",
+	.bus	= BT_HCI_DRIVER_BUS_VIRTUAL,
+	.quirks = BT_QUIRK_NO_AUTO_DLE,
+	.open	= hci_driver_open,
+	.close	= hci_driver_close,
+	.send	= hci_driver_send,
+};
+
+static void swint_handler()
+{
+    if(ll_hci_done_read)
+    {
+        ll_hci_read_callback(ll_hci_read_param,0);
+    }else
+    {
+        ll_hci_write_callback(ll_hci_write_param,0);
+    }
+    ble_ll_task_event_set();
+    z_arm_int_exit();
+}
+
+void ble_sched();
+static void ll_task(void *p1,void *p2,void *p3)
+{
+    while(1)
+    {
+        k_sem_take(&ll_thread_sem,K_FOREVER);
+        ble_sched();
+    }
+}
+
+void aos_swint_init(void (*isr)());
+static int ble_init(const struct device *unused)
+{
+    aos_swint_init(swint_handler);
+    k_thread_create(&ll_thread_data, ll_thread_stack,K_THREAD_STACK_SIZEOF(ll_thread_stack),
+        ll_task,NULL, NULL, NULL,K_PRIO_COOP(LL_THREAD_PRIORITY), 0, K_NO_WAIT);
+    return bt_hci_driver_register(&drv);
+}
+
+SYS_INIT(ble_init, POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEVICE);
---
+++ soc/arm/linkedsemi_arm/le501x/soc.c
@@ -0,0 +1,136 @@
+#include <zephyr/init.h>
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/timer/system_timer.h>
+#include "platform.h"
+
+#define CYC_PER_TICK (sys_clock_hw_cycles_per_sec()	\
+		      / CONFIG_SYS_CLOCK_TICKS_PER_SEC)
+
+#define TIMEOUT_MIN_TICKS 5
+struct hw_time
+{
+    uint32_t hs;
+    uint32_t hus;
+};
+struct hw_time last_anchor;
+struct hw_time target;
+static struct k_spinlock lock;
+
+struct hw_time ble_time_get();
+void ble_timer_set(struct hw_time target);
+void sys_init_ll();
+void ble_ll_init();
+void ke_timer_os_wrapper_init(void (*start)(void **,uint32_t),void (*stop)(void **),void (*tick_handler)(),void (*wakeup_compensate)(uint32_t));
+void os_wrapped_timer_callback(uint32_t param);
+void eif_init(void *read,void *write,void *flow_on,void *flow_off);
+void ll_hci_read(uint8_t *bufptr, uint32_t size, void (*callback)(void *,uint8_t), void* dummy);
+void ll_hci_write(uint8_t *bufptr, uint32_t size, void (*callback)(void *,uint8_t), void* dummy);
+void ll_hci_flow_on();
+bool ll_hci_flow_off();
+void ble_ll_task_event_set();
+void func_post(void (*func)(void *),void *param);
+
+static void timeout_handler(void *param)
+{
+    struct k_timer **timer = param;
+    os_wrapped_timer_callback((uint32_t)timer);
+    k_free(*timer);
+    *timer = NULL;
+}
+
+static void timer_expiry_callback(struct k_timer *timer)
+{
+    void **p_timer = k_timer_user_data_get(timer);
+    func_post(timeout_handler,p_timer);
+    ble_ll_task_event_set();
+}
+
+static void timer_start(void **timer,uint32_t delay)
+{
+    *timer = k_malloc(sizeof(struct k_timer));
+    k_timer_init(*timer,timer_expiry_callback,NULL);
+    k_timer_user_data_set(*timer,timer);
+    k_timer_start(*timer,K_MSEC(delay),K_NO_WAIT);
+}
+
+static void timer_stop(void **timer)
+{
+    k_timer_stop(*timer);
+    k_free(*timer);
+    *timer = NULL;
+}
+
+static void os_tick_handler()
+{
+    struct hw_time current_match = target;
+    sys_clock_announce((((target.hs - last_anchor.hs) & 0xfffffff)*625 - last_anchor.hus + target.hus)/CYC_PER_TICK);
+    last_anchor = current_match;
+}
+
+static void wakeup_compenstation(uint32_t hus)
+{
+
+}
+
+static int le501x_init(const struct device *arg)
+{
+    sys_init_ll();
+    eif_init(ll_hci_read,ll_hci_write,ll_hci_flow_on,ll_hci_flow_off);
+    ke_timer_os_wrapper_init(timer_start,timer_stop,os_tick_handler,wakeup_compenstation);
+    ble_ll_init();
+    return 0;
+}
+
+SYS_INIT(le501x_init, PRE_KERNEL_1, 0);
+
+void ll_stack_reset_hook()
+{
+    last_anchor.hs = 0;
+    last_anchor.hus = 0;
+};
+
+uint32_t sys_clock_elapsed(void)
+{
+    struct hw_time current = ble_time_get();
+    return (((current.hs-last_anchor.hs)&0xfffffff)*625 + current.hus - last_anchor.hus)/CYC_PER_TICK;
+}
+
+void sys_clock_set_timeout(int32_t ticks,bool idle)
+{
+    if(ticks<TIMEOUT_MIN_TICKS) ticks = TIMEOUT_MIN_TICKS;
+    k_spinlock_key_t key = k_spin_lock(&lock);
+    uint32_t step = sys_clock_elapsed()+ticks;
+    target.hus = last_anchor.hus + OSTICK_HUS_STEP_INC(CONFIG_SYS_CLOCK_TICKS_PER_SEC,step);
+    if(target.hus >= 625)
+    {
+        target.hus -= 625;
+        target.hs = (1 + last_anchor.hs + OSTICK_HS_STEP_INC(CONFIG_SYS_CLOCK_TICKS_PER_SEC,step)) & 0xfffffff;
+    }else
+    {
+        target.hs = (last_anchor.hs + OSTICK_HS_STEP_INC(CONFIG_SYS_CLOCK_TICKS_PER_SEC,step)) & 0xfffffff;
+    }
+    ble_timer_set(target);
+	k_spin_unlock(&lock, key);
+}
+
+uint64_t sys_clock_cycle_get_64(void)
+{
+    struct hw_time tim = ble_time_get();
+    return tim.hs*625+tim.hus;
+}
+
+uint32_t sys_clock_cycle_get_32()
+{
+    return sys_clock_cycle_get_64();
+}
+
+uint32_t ble_isr();
+void ble_stack_isr()
+{
+    uint32_t irq_stat = ble_isr();
+    if(irq_stat != 0x40)
+    {
+        ble_ll_task_event_set();
+    }
+    z_arm_int_exit();
+}
