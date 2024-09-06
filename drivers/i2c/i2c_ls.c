9b7cd31044dfc650afb27f540fa5d8bf0dca6307
Hunter Liu <htliu@linkedsemi.com>
2024-09-06 11:01:06 +0800

    clear smbus quick command reg after quick read

--- drivers/i2c/i2c_ls.c
+++ drivers/i2c/i2c_ls.c
@@ -236,6 +236,7 @@ static int i2c_ls_transfer(const struct device *dev, struct i2c_msg *msg,
 			}
 			config->reg->CR2_0_1 = cr2_0_1|I2C_CR2_START_MASK|I2C_CR2_STOP_MASK;
 			k_sem_take(&data->device_sync_sem, K_FOREVER);
+			config->reg->CR2_3 &= ~0x30;
 			if(data->errs)
 			{
 				ret = -EIO;
