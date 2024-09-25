8595e5be5ea722b4547afb5a333386c01d1be373
Hunter Liu <htliu@linkedsemi.com>
2024-09-25 22:13:27 +0800

    lsqsh: use SOC_SERIES_LSQSH

---
+++ soc/linkedsemi/lsqsh/soc_clock.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2024 Linkedsemi Corporation.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _LS_SOC_CLOCK_H_
+#define _LS_SOC_CLOCK_H_
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include <zephyr/devicetree.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define DT_HAS_CLOCKS(inst)    DT_NODE_HAS_PROP(DT_DRV_INST(inst), clocks)
+
+struct ls_clk_cfg {
+	const struct device *cctl_dev;
+	uint16_t cctl_addr_offest:5;
+	uint16_t set_bit:5;
+	uint16_t clr_bit:5;
+};
+
+#define LS_DT_CLK_CFG_ITEM(inst)                                             \
+	{                                                                      \
+	  .cctl_dev = DEVICE_DT_GET(DT_PHANDLE_BY_IDX(DT_DRV_INST(inst),clocks,0)), \
+	  .cctl_addr_offest = DT_PHA(DT_DRV_INST(inst), clocks, cctl_addr_offest),         \
+	  .set_bit  = DT_PHA(DT_DRV_INST(inst), clocks, set_bit),                      \
+	  .clr_bit  = DT_PHA(DT_DRV_INST(inst), clocks, clr_bit),                      \
+	}
+	
+#ifdef __cplusplus
+}
+#endif
+#endif /* _LS_SOC_CLOCK_H_ */
