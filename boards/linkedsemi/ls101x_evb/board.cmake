d1a008fe0a6c9d42c8c2243e03800daf70f90c29
竞超 施 <jcshi@linkedsemi.com>
2024-10-30 14:21:25 +0800

    Modify the copyright name of the files of the linkedsemi

--- boards/linkedsemi/ls101x_evb/board.cmake
+++ boards/linkedsemi/ls101x_evb/board.cmake
@@ -1,4 +1,3 @@
-# Copyright (c) 2021, Teslabs Engineering S.L.
 # SPDX-License-Identifier: Apache-2.0
 
 board_runner_args(jlink "--iface=cJTAG" "--device=LS1010" "--speed=8000" "--erase" "--tool-opt=-JTAGConf -1,-1" "--reset-after-load")
--- soc/linkedsemi/ls101x/soc.h
+++ soc/linkedsemi/ls101x/soc.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Henrik Brix Andersen <henrik@brixandersen.dk>
+ * Copyright (c) 2020 Linkedsemi
  *
  * SPDX-License-Identifier: Apache-2.0
  */
