5d28d4a58c8a26827f5a3306fd3319b9dc20c2bb
Hunter Liu <htliu@linkedsemi.com>
2024-10-30 15:23:58 +0800

    lsqsh: use cpuclusters attr in soc.yml

--- boards/linkedsemi/lsqsh_evb/board.cmake
+++ boards/linkedsemi/lsqsh_evb/board.cmake
@@ -1,6 +1,2 @@
-# Copyright (c) 2021, Teslabs Engineering S.L.
+# Copyright (c) 2024, linkedsemi
 # SPDX-License-Identifier: Apache-2.0
-
-board_runner_args(jlink "--iface=cJTAG" "--device=LSQSH" "--speed=8000" "--erase" "--tool-opt=-JTAGConf -1,-1" "--reset-after-load")
-
-include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
