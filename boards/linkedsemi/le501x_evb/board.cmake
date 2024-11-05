078b7088d97c62f1c437f686ccf3725c2f1d1e4f
pengyang qiu <pyqiu@linkedsemi.com>
2024-11-05 14:02:53 +0800

    boards: linkedsemi: le501x_evb: remove "--erase" from default arguments

--- boards/linkedsemi/le501x_evb/board.cmake
+++ boards/linkedsemi/le501x_evb/board.cmake
@@ -2,5 +2,5 @@
 
 set_property(TARGET runners_yaml_props_target PROPERTY hex_file "production.hex")
 
-board_runner_args(jlink "--device=LE501X" "--speed=4000" "--erase" "--reset-after-load")
+board_runner_args(jlink "--device=LE501X" "--speed=4000" "--reset-after-load")
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
