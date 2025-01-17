9cbb57e0cc0548bd86a12c1f3977e26f4e8bacc9
Bai Kun <kbai@kunluntech.com.cn>
2025-01-17 11:04:58 +0800

    test patch

--- kernel/init_static.c
+++ kernel/init_static.c
@@ -72,6 +72,7 @@ typedef void (*func_ptr)(void);
 extern func_ptr __zephyr_init_array_start[];
 extern func_ptr __zephyr_init_array_end[];
 
+#include <zephyr/kernel.h>
 /**
  * @brief Execute initialization routines referenced in .init_array section
  */
@@ -80,6 +81,7 @@ void __do_init_array_aux(void)
 	for (func_ptr *func = __zephyr_init_array_start;
 		func < __zephyr_init_array_end;
 		func++) {
+		printk("Init item %#p, func %#p\n", func, *func);
 		(*func)();
 	}
 }
