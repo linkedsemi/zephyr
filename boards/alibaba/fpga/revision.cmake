22537cf663d134285dbe8468e17a6d15accbbecc
Jianqiang,Mei <meijianqiang.mjq@alibaba-inc.com>
2025-02-17 17:13:59 +0800

    Update alimoc30 board configs to fpga version v250213a1
    Signed-off-by: Jianqiang,Mei <meijianqiang.mjq@alibaba-inc.com>

--- boards/alibaba/fpga/revision.cmake
+++ boards/alibaba/fpga/revision.cmake
@@ -1,7 +1,14 @@
+set(BOARD_REVISIONS "zynq"
+                    "acku"
+                    "acku-sc"
+                    "acku-xip")
 if (NOT DEFINED BOARD_REVISION)
   set(BOARD_REVISION "acku")
 else ()
-  if (NOT (BOARD_REVISION STREQUAL "zynq") AND NOT (BOARD_REVISION STREQUAL "acku") AND NOT (BOARD_REVISION STREQUAL "acku-sc"))
-    message(FATAL_ERROR "Invalid board revision, ${BOARD_REVISION}, valid revisions are: zynq, acku, acku-sc")
+  if (NOT (BOARD_REVISION STREQUAL "zynq") 
+      AND NOT (BOARD_REVISION STREQUAL "acku") 
+      AND NOT (BOARD_REVISION STREQUAL "acku-sc")
+      AND NOT (BOARD_REVISION STREQUAL "acku-xip"))
+    message(FATAL_ERROR "Invalid board revision, ${BOARD_REVISION}, valid revisions are: zynq, acku, acku-sc, acku-xip")
   endif()
 endif()
