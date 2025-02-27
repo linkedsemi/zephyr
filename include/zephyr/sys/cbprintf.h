2f4c59d865affafd4bc8fe7652bc5703fa4e0822
Bai Kun <kbai@kunluntech.com.cn>
2025-02-27 14:47:30 +0800

    解决cbprintf大量编译告警问题

--- include/zephyr/sys/cbprintf.h
+++ include/zephyr/sys/cbprintf.h
@@ -134,13 +134,16 @@ extern "C" {
  */
 
 /** @brief Required alignment of the buffer used for packaging. */
-// #ifdef __xtensa__
-// #define CBPRINTF_PACKAGE_ALIGNMENT 16
-// #else
-// #define CBPRINTF_PACKAGE_ALIGNMENT \
-// 	Z_POW2_CEIL(COND_CODE_1(CONFIG_CBPRINTF_PACKAGE_LONGDOUBLE, \
-// 		(sizeof(long double)), (MAX(sizeof(double), sizeof(long long)))))
-// #endif
+#if 0
+#ifdef __xtensa__
+#define CBPRINTF_PACKAGE_ALIGNMENT 16
+#else
+#define CBPRINTF_PACKAGE_ALIGNMENT \
+	Z_POW2_CEIL(COND_CODE_1(CONFIG_CBPRINTF_PACKAGE_LONGDOUBLE, \
+		(sizeof(long double)), (MAX(sizeof(double), sizeof(long long)))))
+#endif
+#endif
+
 #define CBPRINTF_PACKAGE_ALIGNMENT 16
 BUILD_ASSERT(Z_IS_POW2(CBPRINTF_PACKAGE_ALIGNMENT));
 
