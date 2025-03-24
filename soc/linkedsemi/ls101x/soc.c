40db8c766ad01b75b389ddeee546e6477b8efa80
Jc Shi <jcshi@linkedsemi.com>
2025-03-24 13:52:39 +0800

    更新中断嵌套最大层次限制

--- soc/linkedsemi/ls101x/soc.c
+++ soc/linkedsemi/ls101x/soc.c
@@ -15,10 +15,12 @@
 #include "reg_v33_rg.h"
 #include "field_manipulate.h"
 #include "ls_msp_peci.h"
+#include "soc.h"
+
+volatile uint32_t irq_nested_level = 0;
+volatile uint32_t irq_nested_mcause[IRQ_NESTED_MAX] = {0,0,0,0,0,0,0,0,0,0};
 
 #define RV_SOFT_IRQ_IDX 23
-extern void noint(void);
-uint32_t *pTaskStack = NULL;
 
 static void cpu_sleep_mode_config(uint8_t deep)
 {
