19d9da187f0b939e9d91ae30ae7050f62a58d4b8
Jc Shi <jcshi@linkedsemi.com>
2025-03-27 13:38:36 +0800

    zephyr e906 flash irq handle

--- soc/linkedsemi/lsqsh/soc.c
+++ soc/linkedsemi/lsqsh/soc.c
@@ -41,12 +41,6 @@ void sys_arch_reboot(int type)
 	// platform_reset(0);
 }
 
-void Swint_Handler_C(uint32_t *args)
-{
-    uint32_t (*func)(uint32_t,uint32_t,uint32_t,uint32_t) = (void *)args[12];
-    args[8] = func(args[8],args[9],args[10],args[11]);
-}
-
 #if defined(CONFIG_NOCACHE_MEMORY)
 extern uint32_t _nocache_ram_start;
 extern uint32_t _nocache_ram_end;
