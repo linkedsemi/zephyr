782d60a571c705e01b97441d8edd1041a3763ffe
Hunter Liu <htliu@linkedsemi.com>
2025-03-31 22:13:20 +0800

    mbox_linkedsemi_app_cpu1.c: fix bug that move mbox_acquire_cpu2_idle() key body to ram

--- drivers/mbox/mbox_linkedsemi_app_cpu1.c
+++ drivers/mbox/mbox_linkedsemi_app_cpu1.c
@@ -7,6 +7,21 @@
 __nocache static volatile bool g_done = false;
 __nocache static volatile bool g_ack = false;
 
+__ramfunc static void mbox_func_call_send_and_wait(const struct mbox_dt_spec *tx_channel,
+                                                   struct mbox_msg *msg,
+                                                   uint32_t *retry_cnt)
+{
+    if (mbox_send_dt(tx_channel, msg) == -ENOSPC) {
+        printk("mbox_send() full\n");
+    }
+    sys_cache_data_invd_all();
+    *retry_cnt = 0;
+    while ((!g_ack) && (*retry_cnt < MBOX_RETRY_MAX_CNT)) {
+        (*retry_cnt)++;
+        nop_delay(10);
+    }
+}
+
 int mbox_acquire_cpu2_idle(const struct mbox_dt_spec *tx_channel)
 {
     mbox_func_call_data_t mbox_func_call_data = {};
@@ -27,15 +42,7 @@ int mbox_acquire_cpu2_idle(const struct mbox_dt_spec *tx_channel)
     g_done = false;
     g_ack = false;
     sys_cache_data_flush_all();
-    if (mbox_send_dt(tx_channel, &msg) == -ENOSPC) {
-        printk("mbox_send() full\n");
-    }
-    sys_cache_data_invd_all();
-    retry_cnt = 0;
-    while ((!g_ack) && (retry_cnt < MBOX_RETRY_MAX_CNT)) {
-        retry_cnt++;
-        nop_delay(10);
-    }
+    mbox_func_call_send_and_wait(tx_channel, &msg, &retry_cnt);
     if (retry_cnt > MBOX_RETRY_MAX_CNT) {
         ret = -1;
     }
