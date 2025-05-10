8079a5b55e3aafbf7003eb7d295579054fc02fea
Hunter Liu <htliu@linkedsemi.com>
2025-05-10 18:45:12 +0800

    samples/linkedsemi/wdt_cpu1: update

--- samples/linkedsemi/wdt_cpu1/src/main.c
+++ samples/linkedsemi/wdt_cpu1/src/main.c
@@ -5,15 +5,20 @@
  */
 
 #include <stdio.h>
+#include <zephyr/kernel.h>
 #include <ls_hal_iwdgv2.h>
 
-#define BOOT_WDG_VALUE_BASE_S  (1000000 * 10)
-#define BOOT_WDG_VALUE_BASE_MS ((1000000 * 10) / 1000)
+#define BOOT_WDG_VALUE_BASE_S  (32768)
+#define BOOT_WDG_VALUE_BASE_MS ((32768) / 1000)
 
 int main(void)
 {
     printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
     HAL_IWDG_Init(SEC_IWDG, BOOT_WDG_VALUE_BASE_S * 2);
+    while(1) {
+        printf("wait for reset..\n");
+        k_msleep(300);
+    }
 
     return 0;
 }
