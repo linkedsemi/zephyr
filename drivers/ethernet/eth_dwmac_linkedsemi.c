d84d840965e81b9c442342613381f475385445e8
xieqisen.xqs <xieqisen.xqs@alibaba-inc.com>
2025-05-12 10:28:38 +0800

    Modify LOG_ERR to LOG_ERROR in "eth_dwmac_lilnkedsemi.c"
    Signed-off-by: xieqisen.xqs <xieqisen.xqs@alibaba-inc.com>

--- drivers/ethernet/eth_dwmac_linkedsemi.c
+++ drivers/ethernet/eth_dwmac_linkedsemi.c
@@ -62,13 +62,13 @@ int dwmac_bus_init(struct dwmac_priv *p)
 #if defined(CONFIG_RESET)
     if (dev_config->reset.dev != NULL) {
         if (!device_is_ready(dev_config->reset.dev)) {
-            LOG_ERR("Reset controller device is not ready");
+            LOG_ERROR("Reset controller device is not ready");
             return -ENODEV;
         }
 
         ret = reset_line_toggle(dev_config->reset.dev, dev_config->reset.id);
         if (ret != 0) {
-            LOG_ERR("toggle reset line failed");
+            LOG_ERROR("toggle reset line failed");
             return ret;
         }
     }
