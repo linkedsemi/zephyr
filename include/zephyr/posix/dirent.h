7c6197b143ba6f115d5a29687ad350aff1a7d917
xieqisen.xqs <xieqisen.xqs@alibaba-inc.com>
2025-05-16 16:45:47 +0800

    Addd linux/fs_types in "posix/dirent.h"
    Signed-off-by: xieqisen.xqs <xieqisen.xqs@alibaba-inc.com>

--- include/zephyr/posix/dirent.h
+++ include/zephyr/posix/dirent.h
@@ -19,8 +19,21 @@ extern "C" {
 
 typedef void DIR;
 
+/* From linux/fs_types.h
+   these are defined by POSIX and also present in glibc's dirent.h */
+#define DT_UNKNOWN	0
+#define DT_FIFO		1
+#define DT_CHR		2
+#define DT_DIR		4
+#define DT_BLK		6
+#define DT_REG		8
+#define DT_LNK		10
+#define DT_SOCK		12
+#define DT_WHT		14
+
 struct dirent {
 	unsigned int d_ino;
+	unsigned char d_type;
 	char d_name[PATH_MAX + 1];
 };
 
