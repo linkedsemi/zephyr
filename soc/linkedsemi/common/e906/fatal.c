bb9193016897e529bc53ff57a4a9925fe03db366
Hunter Liu <htliu@linkedsemi.com>
2025-05-20 19:09:15 +0800

    soc/linkedsemi/common/e906/fatal.c: remove useless code

--- soc/linkedsemi/common/e906/fatal.c
+++ soc/linkedsemi/common/e906/fatal.c
@@ -6,32 +6,11 @@
 
 LOG_MODULE_DECLARE(os, CONFIG_KERNEL_LOG_LEVEL);
 
-FUNC_NORETURN void arch_system_halt(unsigned int reason)
-{
-    ARG_UNUSED(reason);
-
-    /* TODO: What's the best way to totally halt the system if SMP
-     * is enabled?
-     */
-
-    (void)arch_irq_lock();
-    for (;;) {
-        /* Spin endlessly */
-    }
-}
-
 void k_sys_fatal_error_handler(unsigned int reason,
-                      const struct arch_esf *esf)
+                               const struct arch_esf *esf)
 {
     ARG_UNUSED(esf);
 
     LOG_PANIC();
-    LOG_ERROR("Halting thread");
-    if (IS_ENABLED(CONFIG_MULTITHREADING)) {
-        discard_current_irq_nested();
-        k_thread_abort(_current);
-    } else {
-        arch_system_halt(reason);
-    }
-    CODE_UNREACHABLE;
+    LOG_ERR("not Halting system");
 }
