ddd4fab5d3a5a5d355b2697d2dd98dd450a4af0e
Hunter Liu <htliu@linkedsemi.com>
2025-06-24 14:51:12 +0800

    soc/linkedsemi/common/e906/fatal.c: halting system if exception occur after isr

--- soc/linkedsemi/common/e906/fatal.c
+++ soc/linkedsemi/common/e906/fatal.c
@@ -2,6 +2,7 @@
 #include <zephyr/logging/log_ctrl.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/fatal.h>
+#include <cpu.h>
 #include "soc_isr_stacking_e906.h"
 
 LOG_MODULE_DECLARE(os, CONFIG_KERNEL_LOG_LEVEL);
@@ -12,5 +13,13 @@ void k_sys_fatal_error_handler(unsigned int reason,
     ARG_UNUSED(esf);
 
     LOG_PANIC();
-    LOG_ERR("not Halting system");
+    if (irq_nested_level > 1) {
+        LOG_ERR("fatal error! Halting system");
+        disable_global_irq();
+        for (;;) {;}
+        CODE_UNREACHABLE;
+    } else {
+        // irq_nested_level--;
+        LOG_ERR("not Halting system");
+    }
 }
