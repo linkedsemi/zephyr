acc46f8d1a925e60ccde8bacb346178bad8f174c
Hunter Liu <htliu@linkedsemi.com>
2025-06-27 14:01:12 +0800

    samples/linkedsemi/reboot_cpu_simple: new sample

---
+++ samples/linkedsemi/reboot_cpu_simple/src/main.c
@@ -0,0 +1,22 @@
+#include <zephyr/kernel.h>
+#include <zephyr/cache.h>
+#include <stdio.h>
+#include <cpu.h>
+#include <zephyr/linker/linker-defs.h>
+
+int main(void)
+{
+    printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+
+    k_msleep(500);
+
+    disable_global_irq();
+    sys_cache_data_flush_all();
+    sys_cache_data_disable();
+    sys_cache_instr_disable();
+    void (* goto_sbl)();
+    goto_sbl = (void *)__rom_region_start;
+    goto_sbl();
+
+    return 0;
+}
