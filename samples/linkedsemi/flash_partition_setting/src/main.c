5ea78130b51f2c8ef9f5baeed839def3a01a67a0
Hunter Liu <htliu@linkedsemi.com>
2025-07-10 17:08:57 +0800

    samples/linkedsemi/flash_partition_setting: optimize

--- samples/linkedsemi/flash_partition_setting/src/main.c
+++ samples/linkedsemi/flash_partition_setting/src/main.c
@@ -18,6 +18,7 @@ int main(void)
     uint32_t size = 0;
     uint32_t attr = 0;
 
+    /* show static layout info */
     for (int idx = 0; idx < flash_ls_get_part_num(dev); idx++) {
         flash_ls_get_part_info(dev, idx, &base, &size, &attr);
         printf("idx: %d\n", idx);
@@ -26,6 +27,7 @@ int main(void)
         printf("    attr: %x\n", attr);
     }
 
+    /* runtime set layout attr to 0 that app cpu can not read/write flash any more */
     for (int idx = 0; idx < flash_ls_get_part_num(dev); idx++) {
         flash_ls_set_part_attr(dev, idx, 0);
         flash_ls_get_part_info(dev, idx, &base, &size, &attr);
@@ -35,8 +37,9 @@ int main(void)
         printf("    attr: %x\n", attr);
     }
 
+    /* runtime set layout attr to PMP_R | PMP_W that app cpu can normally read/write flash */
     for (int idx = 0; idx < flash_ls_get_part_num(dev); idx++) {
-        flash_ls_set_part_attr(dev, idx, 7);
+        flash_ls_set_part_attr(dev, idx, PMP_R | PMP_W);
         flash_ls_get_part_info(dev, idx, &base, &size, &attr);
         printf("idx: %d\n", idx);
         printf("    base: %x\n", base);
