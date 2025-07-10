6491abdcd724d6349b281683a804bc2522cfdae6
Hunter Liu <htliu@linkedsemi.com>
2025-07-10 16:57:22 +0800

    drivers/flash/soc_flash_ls.c: add flash_ls_get_part_num()

--- include/zephyr/drivers/flash/soc_flash_ls.h
+++ include/zephyr/drivers/flash/soc_flash_ls.h
@@ -12,6 +12,12 @@
 int flash_ls_get_part_info(const struct device *dev, uint8_t idx, uint32_t *base,
                    uint32_t *size, uint32_t *attr);
 
+/** \brief flash_ls_get_part_num
+ *  \param[in] dev flash device
+ *  \return partition total number
+ */
+int flash_ls_get_part_num(const struct device *dev);
+
 /** \brief flash_ls_set_part_info
  *  \param[in] dev flash device
  *  \param[in] idx partition table index. start from 0
