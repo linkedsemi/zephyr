54b556d40fa3391889eba9ad3300db0ec610f9e4
Hunter Liu <htliu@linkedsemi.com>
2025-08-17 15:57:09 +0800

    drivers/pinctrl/pinctrl_lsqsh.c: add drive-strength config support

--- drivers/pinctrl/pinctrl_lsqsh.c
+++ drivers/pinctrl/pinctrl_lsqsh.c
@@ -71,7 +71,7 @@ static int pinctrl_configure_pin(const pinctrl_soc_pin_t pin_desc)
     }
 
     /* only has effect if mode is push_pull */
-    // io_drive_capacity_write(pin, pin_desc.pin_attr.drive);
+    io_drive_capacity_write(pin, pin_desc.pin_attr.drive);
 
     if (pin_desc.pin_attr.gpio) {
         per_func_disable_all(pin);
