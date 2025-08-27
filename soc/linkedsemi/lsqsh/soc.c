e3c3a737070705dc2c872757aa8f82189ef5762a
yuye <meijianqiang.mjq@alibaba-inc.com>
2025-08-27 14:26:39 +0800

    fix(led-state-ctl): resolve cpu1 led build problem
    Signed-off-by: yuye <meijianqiang.mjq@alibaba-inc.com>

--- soc/linkedsemi/lsqsh/soc.c
+++ soc/linkedsemi/lsqsh/soc.c
@@ -10,6 +10,7 @@
 #include <zephyr/drivers/flash.h>
 #include <zephyr/storage/flash_map.h>
 #include <zephyr/drivers/misc/linkedsemi/mbox_linkedsemi.h>
+#include <zephyr/drivers/led/led_gpio_corelynx.h>
 #include "platform.h"
 #include "core_rv32.h"
 #include "exception_isr.h"
