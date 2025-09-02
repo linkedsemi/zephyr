d03cc63e0a55ddc4771a0c2a2d7b01922b2b570d
yuye <meijianqiang.mjq@alibaba-inc.com>
2025-09-02 17:27:30 +0800

    Revert "lsqsh: set red card mac delay"
    This reverts commit 706f2dfca134d99fbcf6beec8ffc163efdc1191f.

--- soc/linkedsemi/lsqsh/mdio_phy_init.c
+++ soc/linkedsemi/lsqsh/mdio_phy_init.c
@@ -14,8 +14,6 @@ LOG_MODULE_REGISTER(LOG_MODULE_NAME);
 #define BMSR_LINK_STATUS_MASK   0x4
 #define BMSR_LINK_STATUS_POS    2
 #define BMSR_LINK_STATUS_LINKED 0x1
-#define AUTO_NEGOTIATION_COMPLETE_MASK 0x20
-#define AUTO_NEGOTIATION_COMPLETE_POS 5
 
 #define PHYSR                    0x1a
 #define PHYSR_SPEED_STATUS_MASK  0x30
@@ -29,14 +27,76 @@ LOG_MODULE_REGISTER(LOG_MODULE_NAME);
 
 extern void dwmac_10M_100M_speed_cofig(const struct device *const dev);
 extern void dwmac_1000M_2500M_speed_cofig(const struct device *const dev);
-extern void dwmac_tx_unlock(const struct device *dev);
 
 static int mdio_set_phy(void)
 {
+    const struct device *const mdio_dev = DEVICE_DT_GET(DT_NODELABEL(mdio1));
     const struct device *const eth_dev = DEVICE_DT_GET(DT_NODELABEL(eth1));
+    uint16_t val = 0;
+    uint16_t addr = 0x0;
+    uint16_t reg = 0x0;
+    int rc = 0;
 
-    dwmac_1000M_2500M_speed_cofig(eth_dev);
-    return 0;
+    for (int i = 0; i < 32; i++) {
+        if (mdio_read(mdio_dev, i, 0x0, &val) >= 0 && val != UINT16_MAX) {
+            addr = i;
+            break;
+        }
+    }
+
+#if defined(CONFIG_NET_PHY_FORCE_10M)
+    /* force 10Mbps */
+    reg = BMCR;
+    rc = mdio_write(mdio_dev, addr, reg, BMCR_FULL_DUPLEX_OPERATION);
+#elif defined(CONFIG_NET_PHY_FORCE_100M)
+    /* force 100Mbps */
+    reg = BMCR;
+    rc = mdio_write(mdio_dev, addr, reg, BIT(13) | BMCR_FULL_DUPLEX_OPERATION);
+#elif defined(CONFIG_NET_PHY_FORCE_1000M)
+    /* force 1Gbps */
+    reg = BMCR;
+    rc = mdio_write(mdio_dev, addr, reg, BIT(6) | BIT(13) | BMCR_FULL_DUPLEX_OPERATION);
+#else
+    /* auto-negotiation */ 
+    reg = BMCR;
+    rc = mdio_write(mdio_dev, addr, reg, BMCR_RESET_MASK);
+#endif
+
+    do {
+        k_msleep(100);
+        reg = BMCR;
+        rc = mdio_read(mdio_dev, addr, reg, &val);
+        LOG_DBG("phy addr: %d  reg: %x : %4.4x\n", addr, reg, val);
+
+        reg = BMSR;
+        rc = mdio_read(mdio_dev, addr, reg, &val);
+        LOG_DBG("phy addr: %d  reg: %x : %4.4x\n", addr, reg, val);
+    } while (((val & BMSR_LINK_STATUS_MASK) >> BMSR_LINK_STATUS_POS) != BMSR_LINK_STATUS_LINKED);
+
+    reg = PHYID1;
+    rc = mdio_read(mdio_dev, addr, reg, &val);
+    LOG_DBG("phy addr: %d  reg: %x : %4.4x\n", addr, reg, val);
+    reg = PHYID2;
+    rc = mdio_read(mdio_dev, addr, reg, &val);
+    LOG_DBG("phy addr: %d  reg: %x : %4.4x\n", addr, reg, val);
+
+    reg = PHYSR;
+    rc = mdio_read(mdio_dev, addr, reg, &val);
+    LOG_DBG("phy addr: %d  reg: %x : %4.4x\n", addr, reg, val);
+
+    val = (val & PHYSR_SPEED_STATUS_MASK) >> PHYSR_SPEED_STATUS_POS;
+    if (PHYSR_SPEED_STATUS_1000M == val) {
+        dwmac_1000M_2500M_speed_cofig(eth_dev);
+        LOG_INF("1000Mbps link up\n");
+    } else if (PHYSR_SPEED_STATUS_100M == val) {
+        dwmac_10M_100M_speed_cofig(eth_dev);
+        LOG_INF("100Mbps link up\n");
+    } else if (PHYSR_SPEED_STATUS_10M == val) {
+        dwmac_10M_100M_speed_cofig(eth_dev);
+        LOG_INF("10Mbps link up\n");
+    }
+
+    return rc;
 }
 
 SYS_INIT(mdio_set_phy, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
