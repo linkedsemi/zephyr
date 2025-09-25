8970b1748e29cd624ad8a6c3a7d01d1b695b2aa0
qiao zekun <zkqiao@linkedsemi.com>
2025-09-25 03:26:11 +0000

    vuart  & espi code

--- soc/linkedsemi/common/vuart_ls.h
+++ soc/linkedsemi/common/vuart_ls.h
@@ -8,7 +8,7 @@
 typedef void (*ls_vuart_peer_evt_cb_t)(void *dev);
 
 int ls_vuart_get_tx_char(const struct device *dev, uint8_t *out_char);
-bool ls_vuart_tx_ready(const struct device *dev);
+bool host_vuart_rx_available(const struct device *dev);
 void ls_vuart_put_rx_char(const struct device *dev, uint8_t c);
 void ls_vuart_register_tx_start_callback(const struct device *dev, ls_vuart_peer_evt_cb_t callback,
 					 void *user_data);
