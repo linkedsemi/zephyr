11f2144552cc6e664cdd828a9906f29aa790c58f
zxtxin <xtzhang@linkedsemi.com>
2025-09-25 10:56:32 +0800

    initial commit

---
+++ soc/linkedsemi/lsraptor/soc.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2020 Linkedsemi
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _SOC_H_
+#define _SOC_H_
+
+#include "soc_common.h"
+#define IRQ_NESTED_MAX 10
+
+#endif /* _SOC_H_ */
