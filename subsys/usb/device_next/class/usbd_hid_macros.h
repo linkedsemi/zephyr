0fa3c5de58269fdc7c74d7c666422db55cf2fc69
tzy <zytang@linkedsemi.com>
2025-09-26 08:17:14 +0000

    Mcp2221a

--- subsys/usb/device_next/class/usbd_hid_macros.h
+++ subsys/usb/device_next/class/usbd_hid_macros.h
@@ -198,7 +198,7 @@
 	COND_CODE_1(DT_INST_NODE_HAS_PROP(n, out_report_size),			\
 		    (UDC_BUF_POOL_DEFINE(hid_buf_pool_out_##n,			\
 					 CONFIG_USBD_HID_OUT_BUF_COUNT,		\
-					 DT_INST_PROP(n, out_report_size),	\
+					 64,	\
 					 sizeof(struct udc_buf_info), NULL)),	\
 		    ())
 
