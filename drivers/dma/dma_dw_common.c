c01ac9be87ef7c0da1c37b6b841ebcfc7fd2d25f
Hunter Liu <htliu@linkedsemi.com>
2025-10-07 11:51:43 +0800

    drivers/dma/dma_dw_common.c: set lli pool to nocache memory

--- drivers/dma/dma_dw_common.c
+++ drivers/dma/dma_dw_common.c
@@ -215,6 +215,7 @@ int dw_dma_config(const struct device *dev, uint32_t channel,
 	chan_data->cfg_lo = 0;
 	chan_data->cfg_hi = 0;
 
+	__ASSERT_NO_MSG((NULL != dev_data->lli_pool) && IS_ALIGNED((uintptr_t)dev_data->lli_pool, 64));
 	/* setup a list of lli structs. we don't need to allocate */
 	chan_data->lli = &dev_data->lli_pool[channel][0]; /* TODO allocate here */
 	chan_data->lli_count = cfg->block_count;
