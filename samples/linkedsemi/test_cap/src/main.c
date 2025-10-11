65aa561e1dbd7438ccc517c5e12738502093a8bf
qiao zekun <zkqiao@linkedsemi.com>
2025-10-11 08:56:18 +0000

    update cap driver support qsh hardware

--- samples/linkedsemi/test_cap/src/main.c
+++ samples/linkedsemi/test_cap/src/main.c
@@ -7,7 +7,6 @@
 #define PA_NODE DT_NODELABEL(gpioa)
 #define PA1_PIN 1
 
-
 void pulse_task(void *arg1, void *arg2, void *arg3)
 {
 	const struct device *gpioa = DEVICE_DT_GET(PA_NODE);
@@ -16,7 +15,7 @@ void pulse_task(void *arg1, void *arg2, void *arg3)
 	while (1) {
 		gpio_pin_set(gpioa, PA1_PIN, level);
 		level = !level;
-		k_msleep(5); 
+		k_msleep(5);
 	}
 }
 
@@ -33,30 +32,34 @@ void main(void)
 	}
 	printk("CAP device found and ready\n");
 
-
-	k_thread_create(&pulse_thread, pulse_stack, K_THREAD_STACK_SIZEOF(pulse_stack),
-					pulse_task, NULL, NULL, NULL, 7, 0, K_NO_WAIT);
-
+	k_thread_create(&pulse_thread, pulse_stack, K_THREAD_STACK_SIZEOF(pulse_stack), pulse_task,
+			NULL, NULL, NULL, 7, 0, K_NO_WAIT);
+#if defined(CONFIG_CAP_V2)
+	for (int i = 0; i < 16; i++) {
+#else
 	for (int i = 0; i < 8; i++) {
-		printk("[通道%d] cap_sample_fetch: 等待数据...\n", i);
+#endif
+		printk("[channel%d] cap_sample_fetch: waiting for data...\n", i);
 		enum sensor_channel my_channel = (SENSOR_CHAN_CAP_01 + i);
 		int ret = sensor_sample_fetch_chan(cap_dev, my_channel);
 		if (ret) {
-			printk("[通道%d] sensor_sample_fetch 失败: %d\n", i, ret);
+			printk("[channel%d] sensor_sample_fetch failed: %d\n", i, ret);
 			continue;
 		}
-		printk("[通道%d] cap_sample_fetch: 获取到数据!\n", i);
+		printk("[channel%d] cap_sample_fetch: get data!\n", i);
 		struct sensor_value val;
 		ret = sensor_channel_get(cap_dev, my_channel, &val);
 		if (ret) {
-			printk("[通道%d] sensor_channel_get 失败: %d\n", i, ret);
+			printk("[channel%d] sensor_channel_get failed: %d\n", i, ret);
 		} else {
-			printk("[通道%d] 频率: %d Hz, val2: %d\n", i, val.val1, val.val2);
+			printk("[channel%d] frequency: %d Hz, val2: %d\n", i, val.val1, val.val2);
 		}
 		k_msleep(100);
 	}
-	printk("CAP单线程通道0-7测试完成!\n");
+
+#if defined(CONFIG_CAP_V2)
+	printk("CAP channel 0-15 test finished!\n");
+#else
+	printk("CAP channel 0-7 test finished!\n");
+#endif
 }
-
-
-
