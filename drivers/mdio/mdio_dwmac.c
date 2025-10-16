5df154cbf73467ea194fb9b513acea986f2d4adb
Hunter Liu <htliu@linkedsemi.com>
2025-10-16 11:31:25 +0800

    drivers/mdio/mdio_dwmac.c: fix build error due to no phy

--- drivers/mdio/mdio_dwmac.c
+++ drivers/mdio/mdio_dwmac.c
@@ -91,7 +91,7 @@ struct mdio_dwmac_config {
     IF_ENABLED(CONFIG_PINCTRL, (const struct pinctrl_dev_config *pcfg;))
     IF_ENABLED(CONFIG_CLOCK_CONTROL, (struct ls_clk_cfg ccfg;))
     IF_ENABLED(CONFIG_RESET, (struct reset_dt_spec reset;))
-    bool mac_enabled;
+    bool mac_enabled[1];
 };
 
 static bool check_busy(const struct device *dev)
@@ -293,7 +293,7 @@ static int mdio_dwmac_init(const struct device *dev)
     }
 #endif
 
-    if (dev_config->mac_enabled) {
+    if (dev_config->mac_enabled[0]) {
         /* resets all of the MAC internal registers and logic */
         sys_write32(DMA_MODE_SWR, dev_config->base + DMA_MODE);
         timeout = sys_timepoint_calc(K_MSEC(1000));
@@ -347,13 +347,13 @@ static const struct mdio_driver_api mdio_dwmac_api = {
             DT_NODE_HAS_PROP(DT_INST_PHANDLE(inst, clocks), clock_frequency),                                                            \
             (DT_INST_PROP_BY_PHANDLE(inst, clocks, clock_frequency)),                                                                    \
             (DT_INST_PROP(inst, clock_frequency))),                                                                                      \
+        .mac_enabled = {DT_INST_FOREACH_CHILD_STATUS_OKAY_SEP(inst, CHECK_MAC_CHILD_, (||))},                                            \
         IF_ENABLED(DT_ANY_INST_HAS_PROP_STATUS_OKAY(reset_gpios), (.reset_gpio = GPIO_DT_SPEC_INST_GET_OR(inst, reset_gpios, {0}),))     \
         IF_ENABLED(DT_ANY_INST_HAS_PROP_STATUS_OKAY(reset_hold_ms), (.reset_hold_ms = DT_INST_PROP_OR(inst, reset_hold_ms, {0}),))       \
         IF_ENABLED(DT_ANY_INST_HAS_PROP_STATUS_OKAY(circuits_set_ms), (.circuits_set_ms = DT_INST_PROP_OR(inst, circuits_set_ms, {0}),)) \
         IF_ENABLED(CONFIG_PINCTRL, (.pcfg = PINCTRL_DT_INST_DEV_CONFIG_GET(inst), ))                                                     \
         IF_ENABLED(DT_HAS_CLOCKS(inst), (.ccfg = LS_DT_CLK_CFG_ITEM(inst), ))                                                            \
         IF_ENABLED(DT_INST_NODE_HAS_PROP(inst, resets), (.reset = RESET_DT_SPEC_INST_GET(inst), ))                                       \
-        .mac_enabled = DT_INST_FOREACH_CHILD_STATUS_OKAY_SEP(inst, CHECK_MAC_CHILD_, (||)),                                              \
     };                                                                                                                                   \
     DEVICE_DT_INST_DEFINE(inst,                                                                                                          \
                           &mdio_dwmac_init,                                                                                              \
