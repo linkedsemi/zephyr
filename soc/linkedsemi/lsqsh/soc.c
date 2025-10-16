61fda944312dd921c86d57c163be7f904dc434ea
yuye <meijianqiang.mjq@alibaba-inc.com>
2025-10-16 23:02:54 +0800

    fix(dwmac): resolve boot hang caused by dwmac driver
    Signed-off-by: yuye <meijianqiang.mjq@alibaba-inc.com>

--- soc/linkedsemi/lsqsh/soc.c
+++ soc/linkedsemi/lsqsh/soc.c
@@ -200,13 +200,16 @@ void iopmp_region_init(void)
     ls_clock_control_on(IOPMP_CLOCK);
     for (uint32_t idx = 0; idx < 2; idx++) {
         uint32_t dev = SEC_IOPMP1_ADDR + (idx * 0x400);
-        iopmp_config_region_napot4(dev, 0, 0x10000000, KB(512), false, false, false, false);
-        iopmp_config_region_napot4(dev, 1, 0x40002800, KB(1), true, true, true, false);
-        iopmp_config_region_napot4(dev, 2, 0x40005000, KB(1), true, true, true, false);
-        iopmp_config_region_napot4(dev, 3, SEC_SYSC_CPU_SEC_ADDR + 0x28 /* sec_cpu_intr */, 4, true, true, true, false);
-        iopmp_config_region_napot4(dev, 4, 0x40028000, KB(8), true, true, true, false);
+        iopmp_config_region_napot4(dev, 0, 0x1000000, KB(64), false, false, false, false);
+#if 0
+        iopmp_config_region_napot4(dev, 1, 0x8000000, MB(2), false, false, false, false);
+#endif
+        iopmp_config_region_napot4(dev, 2, 0x10060000, KB(128), true, true, true, false);
+        iopmp_config_region_napot4(dev, 3, 0x10000000, KB(512), false, false, false, false);
+        iopmp_config_region_napot4(dev, 4, SEC_SYSC_CPU_SEC_ADDR + 0x28 /* sec_cpu_intr */, 4, true, true, true, false);
         iopmp_config_region_napot4(dev, 5, 0x40000000, KB(256), false, false, false, false);
         iopmp_config_region_napot4(dev, 6, 0x400A0000, KB(32), false, false, false, false);
+
         iopmp_config_region_napot4(dev, 7, 0x0, (uint64_t)4 * GB(1), true, true, true, false);
         iopmp_config_enable(dev, true);
     }
@@ -381,14 +384,16 @@ __maybe_unused static void peripheral_init()
     REG_FIELD_WR(SYSC_APP_AWO->ETH1_CLK_CFG, SYSC_APP_AWO_ETH1_CLK_RX_DIV, 0); /* div = 2 */
     REG_FIELD_WR(SYSC_APP_AWO->ETH1_CLK_CFG, SYSC_APP_AWO_ETH1_CLK_RX_SEL, 0x2); /* rxck pad */
     SET_BIT(SYSC_APP_AWO->ETH1_CLK_CFG, SYSC_APP_AWO_ETH1_CLK_RX_CG_MASK);
+    /* SYSC_APP_AWO->ETH1_CLK_CFG */
 
+    /* SYSC_APP_AWO->ETH2_CLK_CFG */
     REG_FIELD_WR(SYSC_APP_AWO->ETH2_CLK_CFG, SYSC_APP_AWO_ETH2_CLK_TX_DIV, 0);
     REG_FIELD_WR(SYSC_APP_AWO->ETH2_CLK_CFG, SYSC_APP_AWO_ETH2_CLK_TX_SEL, 0x2); /* rxck pad */
     SET_BIT(SYSC_APP_AWO->ETH2_CLK_CFG, SYSC_APP_AWO_ETH2_CLK_TX_CG_MASK);
     REG_FIELD_WR(SYSC_APP_AWO->ETH2_CLK_CFG, SYSC_APP_AWO_ETH2_CLK_RX_DIV, 0); /* div = 2 */
     REG_FIELD_WR(SYSC_APP_AWO->ETH2_CLK_CFG, SYSC_APP_AWO_ETH2_CLK_RX_SEL, 0x2); /* rxck pad */
     SET_BIT(SYSC_APP_AWO->ETH2_CLK_CFG, SYSC_APP_AWO_ETH2_CLK_RX_CG_MASK);
-    /* SYSC_APP_AWO->ETH1_CLK_CFG */
+    /* SYSC_APP_AWO->ETH2_CLK_CFG */
 
 
     /* SYSC_APP_AWO->EMMC1_TX_RX_CLK */
@@ -421,6 +426,11 @@ __maybe_unused static void peripheral_init()
     REG_FIELD_WR(SYSC_APP_CPU->ETH1_PHY_CTRL, SYSC_APP_CPU_ETH1_PHY_INTF_SEL, 0x1); /* rgmii */
     REG_FIELD_WR(SYSC_APP_CPU->ETH1_PHY_CTRL, SYSC_APP_CPU_ETH1_PHY_SEL, 0x1); /* rgmii */
     /* SYSC_APP_CPU->ETH1_PHY_CTRL */
+
+    /* SYSC_APP_CPU->ETH2_PHY_CTRL */
+    REG_FIELD_WR(SYSC_APP_CPU->ETH2_PHY_CTRL, SYSC_APP_CPU_ETH2_PHY_INTF_SEL, 0x1); /* rgmii */
+    REG_FIELD_WR(SYSC_APP_CPU->ETH2_PHY_CTRL, SYSC_APP_CPU_ETH2_PHY_SEL, 0x1); /* rgmii */
+    /* SYSC_APP_CPU->ETH2_PHY_CTRL */
 
 
     /* SYSC_APP_AWO->EMMC2_CORE_TIM_CLK */
@@ -693,7 +703,7 @@ void soc_early_init_hook(void)
     sys_write32(0x0, APP_PMU_RG_APP_ADDR + 0x3e8);
 #endif
 
-#if defined(CONFIG_PSRAM)
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(cpu1), okay) && defined(CONFIG_PSRAM)
     if (!is_app_cpu_running()) {
         psram_init();
     }
