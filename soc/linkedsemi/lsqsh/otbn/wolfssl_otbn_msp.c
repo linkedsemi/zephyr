8a4618a63c3f30abb99ba2c9ca00a590943042ee
yuye <meijianqiang.mjq@alibaba-inc.com>
2025-11-10 16:02:57 +0800

    fix(trng): fix build error caused by trng usage on cpu2
    Signed-off-by: yuye <meijianqiang.mjq@alibaba-inc.com>

--- soc/linkedsemi/lsqsh/otbn/wolfssl_otbn_msp.c
+++ soc/linkedsemi/lsqsh/otbn/wolfssl_otbn_msp.c
@@ -3,13 +3,14 @@
 #include <zephyr/drivers/entropy.h>
 #include <zephyr/sys/printk.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 
-
-const static struct device *trng = DEVICE_DT_GET(DT_NODELABEL(trng0));
+const static struct device *trng = DEVICE_DT_GET_OR_NULL(DT_CHOSEN(zephyr_entropy));
 
 
 int ls_trng_get_random(uint8_t *buf, uint16_t need_size)
 {
-    return entropy_get_entropy(trng, buf, need_size);
+    // return entropy_get_entropy(trng, buf, need_size);
+    return rand();
 }
