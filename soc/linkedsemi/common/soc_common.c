52ed55b919f7cbee92f9ecb1990d60a1cd7cc0e1
Hunter Liu <htliu@linkedsemi.com>
2025-11-13 17:10:29 +0800

    soc/linkedsemi/common/soc_common.c: change busy_poll from 32 bit to 64 bit

--- soc/linkedsemi/common/soc_common.c
+++ soc/linkedsemi/common/soc_common.c
@@ -3,13 +3,13 @@
 
 __ramfunc int busy_poll(bool (*poll_fn)(void *),void *param,uint32_t usec_to_wait)
 {
-	uint32_t start_cycles = k_cycle_get_32();
-	uint32_t cycles_to_wait = k_us_to_cyc_ceil32(usec_to_wait);
+	uint64_t start_cycles = k_cycle_get_64();
+	uint64_t cycles_to_wait = k_us_to_cyc_ceil64(usec_to_wait);
 	int ret;
 	for (;;) {
-		uint32_t current_cycles = k_cycle_get_32();
+		uint64_t current_cycles = k_cycle_get_64();
 
-		/* this handles the rollover on an unsigned 32-bit value */
+		/* this handles the rollover on an unsigned 64-bit value */
 		if ((current_cycles - start_cycles) >= cycles_to_wait) {
 			ret = -ETIME;
 			break;
