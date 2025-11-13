5482c7640d0688da5faca04b4915b6acaccc3112
Hunter Liu <htliu@linkedsemi.com>
2025-11-13 11:42:12 +0800

    soc/linkedsemi/lsqsh: modify soc init

--- soc/linkedsemi/lsqsh/soc.h
+++ soc/linkedsemi/lsqsh/soc.h
@@ -11,5 +11,11 @@
 #include "soc_common.h"
 #define IRQ_NESTED_MAX 10
 
+#define IRQ_TYPE_NONE         0
+#define IRQ_TYPE_EDGE_RISING  1
+#define IRQ_TYPE_EDGE_FALLING 2
+#define IRQ_TYPE_EDGE_BOTH    (IRQ_TYPE_EDGE_FALLING | IRQ_TYPE_EDGE_RISING)
+#define IRQ_TYPE_LEVEL_HIGH   4
+#define IRQ_TYPE_LEVEL_LOW    8
 
 #endif /* _SOC_H_ */
