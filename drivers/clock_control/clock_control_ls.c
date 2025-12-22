111da2c3e18f86296a908ddd98e333ad3e5894b4
ry hou <ryhou@linkedsemi.com>
2025-12-22 02:49:47 +0000

    add clock_get_status port

--- drivers/clock_control/clock_control_ls.c
+++ drivers/clock_control/clock_control_ls.c
@@ -8,7 +8,7 @@
 
 LOG_MODULE_REGISTER(clock_control_ls, LOG_LEVEL_DBG);
 
-#define LS_CLK_SET(base, n)   (*(volatile uint32_t *)((base) + (n)))
+#define LS_CLK_SET(base, n)	(*(volatile uint32_t *)((base) + (n)))
 
 #if(CONFIG_SOC_LS1010)
 #include <zephyr/dt-bindings/clock/ls101x_clock.h>
@@ -26,10 +26,28 @@ struct cctl_ls_cfg {
 	uint32_t reg;
 };
 
-static inline int ls_clock_control_on(const struct device *dev,
-					 clock_control_subsys_t sub_system)
+static inline enum clock_control_status ls_clock_control_get_status(const struct device *dev,
+								 clock_control_subsys_t sub_system)
 {
-	ARG_UNUSED(dev);
+	struct ls_clk_cfg *clk_cfg = (struct ls_clk_cfg *)(sub_system);
+	const struct cctl_ls_cfg *const config = dev->config;
+	if(LS_CLK_SET(config->reg, clk_cfg->cctl_addr_offest) & BIT(clk_cfg->set_bit))
+	{
+		return CLOCK_CONTROL_STATUS_ON;
+	}else
+	{
+		return CLOCK_CONTROL_STATUS_OFF;
+	}
+}
+
+static inline int ls_clock_control_on(const struct device *dev,
+					clock_control_subsys_t sub_system)
+{
+	enum clock_control_status status = ls_clock_control_get_status(dev, sub_system);
+
+	if (status == CLOCK_CONTROL_STATUS_ON) 
+		return -EALREADY;
+
 	struct ls_clk_cfg *clk_cfg = (struct ls_clk_cfg *)(sub_system);
 	const struct cctl_ls_cfg *const config = dev->config;
 	LS_CLK_SET(config->reg, clk_cfg->cctl_addr_offest) = BIT(clk_cfg->set_bit);
@@ -37,10 +55,9 @@ static inline int ls_clock_control_on(const struct device *dev,
 }
 
 static inline int ls_clock_control_off(const struct device *dev,
-					  clock_control_subsys_t sub_system)
+					clock_control_subsys_t sub_system)
 {
-	ARG_UNUSED(dev);
-    struct ls_clk_cfg *clk_cfg = (struct ls_clk_cfg *)(sub_system);
+	struct ls_clk_cfg *clk_cfg = (struct ls_clk_cfg *)(sub_system);
 	const struct cctl_ls_cfg *const config = dev->config;
 	LS_CLK_SET(config->reg, clk_cfg->cctl_addr_offest) = BIT(clk_cfg->clr_bit);
 	return 0;
@@ -62,7 +79,7 @@ static int ls_clock_control_get_rate(const struct device *dev, clock_control_sub
 		*rate = CPU_FREQ / (REG_FIELD_RD(SYSC_AWO->PD_AWO_CLK_CTRL, SYSC_AWO_CLK_SEL_PBUS3) + 1);
 		break;
 	case CLK_SRC_PBUS4:
-		*rate = CPU_FREQ  / (REG_FIELD_RD(SYSC_AWO->PD_AWO_CLK_CTRL, SYSC_AWO_CLK_SEL_PBUS4) + 1);
+		*rate = CPU_FREQ / (REG_FIELD_RD(SYSC_AWO->PD_AWO_CLK_CTRL, SYSC_AWO_CLK_SEL_PBUS4) + 1);
 		break;
 	case CLK_SRC_HBUS:
 		*rate = CPU_FREQ;
@@ -136,18 +153,19 @@ static const struct clock_control_driver_api ls_clock_control_api = {
 	.on = ls_clock_control_on,
 	.off = ls_clock_control_off,
 	.get_rate = ls_clock_control_get_rate,
+	.get_status = ls_clock_control_get_status,
 };
 
 #define LS_CCTL_INIT(index)				\
 static const struct cctl_ls_cfg cctl_ls_cfg_##index = {	\
-	.reg = DT_INST_REG_ADDR(index),   \
+	.reg = DT_INST_REG_ADDR(index),	\
 };	\
 								\
-DEVICE_DT_INST_DEFINE(index,          \
-		    NULL,							\
-		    NULL,							\
-		    NULL, &cctl_ls_cfg_##index,		\
-		    PRE_KERNEL_1,						\
-		    CONFIG_CLOCK_CONTROL_INIT_PRIORITY,	\
-		    &ls_clock_control_api);
+DEVICE_DT_INST_DEFINE(index,	\
+			NULL,							\
+			NULL,							\
+			NULL, &cctl_ls_cfg_##index,		\
+			PRE_KERNEL_1,						\
+			CONFIG_CLOCK_CONTROL_INIT_PRIORITY,	\
+			&ls_clock_control_api);
 DT_INST_FOREACH_STATUS_OKAY(LS_CCTL_INIT)
