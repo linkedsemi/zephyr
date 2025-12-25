0c87c33745154647e26476ee082b3cca8064d998
Hunter Liu <htliu@linkedsemi.com>
2025-12-25 11:55:24 +0800

    soc/linkedsemi/lsqsh/soc.c: add SYS_REBOOT_COLD

--- soc/linkedsemi/lsqsh/soc.c
+++ soc/linkedsemi/lsqsh/soc.c
@@ -65,22 +65,26 @@ static void driver_init(void)
 
 void sys_arch_reboot(int type)
 {
+    if (type == SYS_REBOOT_COLD) {
+        REG_FIELD_WR(SEC_PMU->RST_SFT, SEC_PMU_RG_RST_FROM_SFT, 0x1);
+    } else if (type == SYS_REBOOT_WARM) {
 #if (DT_NODE_HAS_STATUS(DT_NODELABEL(cpu1), okay))
-    disable_global_irq();
-    reset_reason_magic_set();
-    sys_cache_data_flush_all();
-    sys_cache_data_disable();
-    sys_cache_instr_disable();
-    for (int irq = 0; irq < CONFIG_NUM_IRQS; irq++) {
-        irq_disable(irq);
-    }
-    void (* goto_rom_region_start)();
-    goto_rom_region_start = (void *)__rom_region_start;
-    goto_rom_region_start();
+        disable_global_irq();
+        reset_reason_magic_set();
+        sys_cache_data_flush_all();
+        sys_cache_data_disable();
+        sys_cache_instr_disable();
+        for (int irq = 0; irq < CONFIG_NUM_IRQS; irq++) {
+            irq_disable(irq);
+        }
+        void (* goto_rom_region_start)();
+        goto_rom_region_start = (void *)__rom_region_start;
+        goto_rom_region_start();
 #else
-    sys_cache_data_flush_all();
-    csi_core_reset();
+        sys_cache_data_flush_all();
+        csi_core_reset();
 #endif
+    }
 }
 
 #define CPU0_FW_REGION_SIZE MB(2)
