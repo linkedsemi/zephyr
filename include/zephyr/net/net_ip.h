ef87c4f6827885753d147e3b051b002da2a77217
qiaozekun <zkqiao@linkedsemi.com>
2026-03-25 13:55:23 +0800

    update sin6_flowinfo

--- include/zephyr/net/net_ip.h
+++ include/zephyr/net/net_ip.h
@@ -181,6 +181,7 @@ typedef size_t socklen_t;
 struct sockaddr_in6 {
 	sa_family_t		sin6_family;   /**< AF_INET6               */
 	uint16_t		sin6_port;     /**< Port number            */
+	uint32_t		sin6_flowinfo; /**< IPv6 flow information  */
 	struct in6_addr		sin6_addr;     /**< IPv6 address           */
 	uint8_t			sin6_scope_id; /**< Interfaces for a scope */
 };
@@ -209,6 +210,7 @@ struct sockaddr_ll {
 struct sockaddr_in6_ptr {
 	sa_family_t		sin6_family;   /**< AF_INET6               */
 	uint16_t		sin6_port;     /**< Port number            */
+	uint32_t		sin6_flowinfo; /**< IPv6 flow information  */
 	struct in6_addr		*sin6_addr;    /**< IPv6 address           */
 	uint8_t			sin6_scope_id; /**< interfaces for a scope */
 };
