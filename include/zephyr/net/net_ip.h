da66aa08d2cb48e9300849bf043bcd1305e83d80
wzou <wzou@linkedsemi.com>
2026-04-28 10:54:46 +0800

    fix(NET_SOCKADDR_MAX_SIZE): add CONFIG_OPENBMC to control NET_SOCKADDR_MAX_SIZE
    Signed-off-by: wzou <wzou@linkedsemi.com>

--- include/zephyr/net/net_ip.h
+++ include/zephyr/net/net_ip.h
@@ -407,8 +407,10 @@ struct sockaddr_storage {
 };
 
 /* For sd-bus linux compatible definition: sun_path fixed to 108 bytes */
+#ifdef CONFIG_OPENBMC
 #undef NET_SOCKADDR_MAX_SIZE
 #define NET_SOCKADDR_MAX_SIZE (sizeof(sa_family_t) + 108)
+#endif /* CONFIG_OPENBMC */
 
 /* Socket address struct for UNIX domain sockets */
 struct sockaddr_un {
