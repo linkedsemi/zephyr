64f41319762cde49fddd1109243b00cdb6f04ce4
Hunter Liu <htliu@linkedsemi.com>
2026-05-06 23:57:52 -0400

    soc/linkedsemi/lsqsh/heap_debug.c: optimize

--- soc/linkedsemi/lsqsh/heap_debug.c
+++ soc/linkedsemi/lsqsh/heap_debug.c
@@ -16,8 +16,6 @@ enum cmp_op
     CMP_EQ,
 };
 
-#define HEAP_DEBUG_RECORX_MAX_NUM                   (4096)
-
 struct heap_debug_cs_range
 {
     enum cmp_op op1;
@@ -28,27 +26,33 @@ struct heap_debug_cs_range
     bool is_enable;
 };
 
-struct heap_debug_cs_name
+struct heap_debug_thread_name
 {
     char name_filter[CONFIG_THREAD_MAX_NAME_LEN];
+    uint8_t name_filter_len;
     bool is_enable;
 };
 
 struct heap_debug_record
 {
-    const char *name;
-    size_t ptr;
+    uint64_t alloc_moment;
+    const char *thread_name;
+    const char *alloc_name;
+    void *ptr;
     size_t size;
 };
 
 struct heap_debug
 {
     struct heap_debug_cs_range rang;
-    struct heap_debug_cs_name name;
-    struct heap_debug_record record[HEAP_DEBUG_RECORX_MAX_NUM];
-    int rd_index;
-    int rd_enable;
+    struct heap_debug_thread_name thread_name;
     int cs_enable;
+#if defined(CONFIG_HEAP_DEBUG_RD_LSQSH)
+    struct heap_debug_record record[CONFIG_HEAP_DEBUG_RECORD_MAX_NUM];
+    int rd_index;
+    int rd_index_max;
+    int rd_enable;
+#endif /* CONFIG_HEAP_DEBUG_RD_LSQSH */
 };
 
 static struct heap_debug heap_debug;
@@ -59,14 +63,14 @@ static int heap_debug_help(const struct shell *sh, size_t argc, char **argv)
     return 0;
 }
 
-static int heap_debug_cs_enable(const struct shell *sh, size_t argc, char **argv)
+static int cmd_heap_debug_cs_enable(const struct shell *sh, size_t argc, char **argv)
 {
     printf("heap debug enable\n");
     heap_debug.cs_enable = 1;
     return 0;
 }
 
-static int heap_debug_cs_disable(const struct shell *sh, size_t argc, char **argv)
+static int cmd_heap_debug_cs_disable(const struct shell *sh, size_t argc, char **argv)
 {
     printf("heap debug disable\n");
     heap_debug.cs_enable = 0;
@@ -125,12 +129,15 @@ static bool heap_debug_range_match(size_t size)
 
 static bool heap_debug_name_match(const char *name)
 {
-    return !strcmp(name, heap_debug.name.name_filter);
+    if (NULL == name) {
+        return false;
+    }
+    return !memcmp(name, heap_debug.thread_name.name_filter, heap_debug.thread_name.name_filter_len);
 }
 
 static bool heap_debug_match(size_t size, const char *name)
 {
-    if (heap_debug.name.is_enable && heap_debug.rang.is_enable)
+    if (heap_debug.thread_name.is_enable && heap_debug.rang.is_enable)
     {
         return heap_debug_name_match(name) && heap_debug_range_match(size);
     }
@@ -138,7 +145,7 @@ static bool heap_debug_match(size_t size, const char *name)
     {
         return heap_debug_range_match(size);
     }
-    else if (heap_debug.name.is_enable){
+    else if (heap_debug.thread_name.is_enable){
         return heap_debug_name_match(name);
     }
 
@@ -160,7 +167,7 @@ static bool heap_debug_cs_match(size_t size, const char *name)
     [3] = <cmp2> (optional)
     [4] = <val2> (optional)
 */
-static int heap_debug_cs_range(const struct shell *sh, size_t argc, char **argv)
+static int cmd_heap_debug_cs_range(const struct shell *sh, size_t argc, char **argv)
 {
     if (argc == 2)
     {
@@ -168,7 +175,7 @@ static int heap_debug_cs_range(const struct shell *sh, size_t argc, char **argv)
         heap_debug.rang.is_enable = 0;
         return 0;
     }
-    
+
     if (argc != 3 && argc != 5)
     {
         printf("Usage:\n"
@@ -217,18 +224,19 @@ static int heap_debug_cs_range(const struct shell *sh, size_t argc, char **argv)
     return 0;
 }
 
-static int heap_debug_cs_name(const struct shell *sh, size_t argc, char **argv)
+static int heap_debug_thread_name(const struct shell *sh, size_t argc, char **argv)
 {
     if (strcmp(argv[1], "close") == 0)
     {
         printf("disable name filter\n");
-        heap_debug.name.is_enable = 0;
+        heap_debug.thread_name.is_enable = 0;
         return 0;
     }
 
-    snprintf(heap_debug.name.name_filter, CONFIG_THREAD_MAX_NAME_LEN, "%s", argv[1]);
-    printf("heap_debug: cs name set: %s\n", heap_debug.name.name_filter);\
-    heap_debug.name.is_enable = 1;
+    snprintf(heap_debug.thread_name.name_filter, CONFIG_THREAD_MAX_NAME_LEN, "%s", argv[1]);
+    heap_debug.thread_name.name_filter_len = strlen(heap_debug.thread_name.name_filter) + 1;
+    printf("heap_debug: thread_name set: %s\n", heap_debug.thread_name.name_filter);
+    heap_debug.thread_name.is_enable = 1;
 
     return 0;
 }
@@ -239,56 +247,96 @@ static bool print_trace_address(void *arg, unsigned long ra)
     return true;
 }
 
-void heap_debug_callstack(const char *func, size_t ptr, size_t size)
+void heap_debug_callstack(const char *func, void *ptr, size_t size)
 {
     int match = heap_debug_cs_match(size, k_current_get()->name);
     if (match)
     {
         printf("============== call stack start ====================\n");
-        printf("thread: %p, name: %s, func = %s, ptr = %x, size = %u\n", k_current_get(), k_current_get()->name, func, ptr, size);
+        printf("thread: %p, name: %s, func = %s, ptr = %p, size = %u\n", k_current_get(), k_current_get()->name, func, ptr, size);
         arch_stack_walk(print_trace_address, NULL, k_current_get(), NULL);
         printf("============== call stack end ====================\n");
     }
 }
 
-void heap_debug_ptr_push(void *ptr, size_t size)
+#if defined(CONFIG_HEAP_DEBUG_RD_LSQSH)
+void heap_debug_ptr_push(const char *alloc_name, void *ptr, size_t size)
 {
-    if (!heap_debug.rd_enable || heap_debug.rd_index == HEAP_DEBUG_RECORX_MAX_NUM)
+    if (!heap_debug.rd_enable || heap_debug.rd_index == CONFIG_HEAP_DEBUG_RECORD_MAX_NUM)
         return;
 
+    if (heap_debug.thread_name.is_enable)
+    {
+        if (!heap_debug_name_match(k_current_get()->name))
+        {
+            return;
+        }
+    }
     struct heap_debug_record *record = heap_debug.record;
 
     k_mutex_lock(&heap_rd_lock, K_FOREVER);
-    for (int i = 0; i < HEAP_DEBUG_RECORX_MAX_NUM; i++)
+    for (int i = 0; i < CONFIG_HEAP_DEBUG_RECORD_MAX_NUM; i++)
     {
-        if (record[i].name == NULL)
+        if (record[i].thread_name == NULL)
         {
-            record[i].ptr = (size_t)ptr;
-            record[i].name = k_current_get()->name;
+            record[i].ptr = ptr;
+            record[i].thread_name = k_current_get()->name;
+            record[i].alloc_name = alloc_name;
+            record[i].alloc_moment = sys_clock_cycle_get_64();
             record[i].size = size;
+
             heap_debug.rd_index++;
+            if (heap_debug.rd_index_max < heap_debug.rd_index) {
+                heap_debug.rd_index_max = heap_debug.rd_index;
+            }
             break;
         }
     }
     k_mutex_unlock(&heap_rd_lock);
 }
 
-void heap_debug_ptr_pop(void *ptr)
+void heap_debug_ptr_pop(void *ptr, size_t size)
 {
-    if (!heap_debug.rd_enable)
+    if ((!heap_debug.rd_enable) || (0 == heap_debug.rd_index))
         return;
 
     struct heap_debug_record *record = heap_debug.record;
 
     k_mutex_lock(&heap_rd_lock, K_FOREVER);
-    for (int i = 0; i < HEAP_DEBUG_RECORX_MAX_NUM; i++)
+    for (int i = 0; i < MIN(heap_debug.rd_index_max, CONFIG_HEAP_DEBUG_RECORD_MAX_NUM); i++)
     {
-        if (record[i].name)
+        if (record[i].thread_name)
         {
-            if (record[i].ptr == (size_t)ptr)
+            if (record[i].ptr == ptr)
             {
-                record[i].name = NULL;
+                record[i].thread_name = NULL;
                 heap_debug.rd_index--;
+                break;
+            }
+        }
+    }
+    k_mutex_unlock(&heap_rd_lock);
+}
+
+void heap_debug_ptr_replace(void *old_ptr, size_t old_size, const char *alloc_name, void *new_ptr, size_t new_size)
+{
+    if ((!heap_debug.rd_enable) || (0 == heap_debug.rd_index))
+        return;
+
+    struct heap_debug_record *record = heap_debug.record;
+
+    k_mutex_lock(&heap_rd_lock, K_FOREVER);
+    for (int i = 0; i < MIN(heap_debug.rd_index_max, CONFIG_HEAP_DEBUG_RECORD_MAX_NUM); i++)
+    {
+        if (record[i].thread_name)
+        {
+            if (record[i].ptr == old_ptr)
+            {
+                record[i].ptr = new_ptr;
+                record[i].thread_name = k_current_get()->name;
+                record[i].alloc_name = alloc_name;
+                record[i].alloc_moment = sys_clock_cycle_get_64();
+                record[i].size = new_size;
                 break;
             }
         }
@@ -298,43 +346,56 @@ void heap_debug_ptr_pop(void *ptr)
 
 void heap_debug_ptr_dump(void)
 {
+    uint32_t total_count = 0;
+    uint32_t total_size = 0;
     struct heap_debug_record *record = heap_debug.record;
     printf("============ heap ptr record dump =================\n");
-    for (int i = 0; i < HEAP_DEBUG_RECORX_MAX_NUM; i++)
+    for (int i = 0; i < MIN(heap_debug.rd_index_max, CONFIG_HEAP_DEBUG_RECORD_MAX_NUM); i++)
     {
-        if (record[i].name)
+        if (record[i].thread_name)
         {
-            printf("thread: %s, ptr: %x, size: %u\n", record[i].name, record[i].ptr, record[i].size);
+            total_size += record[i].size;
+            total_count++;
+
+            printf("[%d]: thread: %s %s:, ptr: %8p, size: %-10u alloc_moment: %-20llu\n", total_count, record[i].thread_name, record[i].alloc_name, record[i].ptr, record[i].size, record[i].alloc_moment);
         }
     }
+    printf("total_count: %u total_size: %u\n", total_count, total_size);
 }
 
-static int heap_debug_rd_enable(const struct shell *sh, size_t argc, char **argv)
+int heap_debug_rd_enable()
 {
-    printf("heap debug record enable\n");
     heap_debug.rd_enable = 1;
     return 0;
 }
 
-static int heap_debug_rd_disable(const struct shell *sh, size_t argc, char **argv)
+static int cmd_heap_debug_rd_enable(const struct shell *sh, size_t argc, char **argv)
+{
+    printf("heap debug record enable\n");
+    heap_debug_rd_enable();
+    return 0;
+}
+
+static int cmd_heap_debug_rd_disable(const struct shell *sh, size_t argc, char **argv)
 {
     printf("heap debug record disable\n");
     heap_debug.rd_enable = 0;
     return 0;
 }
 
-static int heap_debug_rd_dump(const struct shell *sh, size_t argc, char **argv)
+static int cmd_heap_debug_rd_dump(const struct shell *sh, size_t argc, char **argv)
 {
     heap_debug_ptr_dump();
     return 0;
 }
 
-static int heap_debug_rd_clear(const struct shell *sh, size_t argc, char **argv)
+static int cmd_heap_debug_rd_clear(const struct shell *sh, size_t argc, char **argv)
 {
     printf("heap debug record clear\n");
     k_mutex_lock(&heap_rd_lock, K_FOREVER);
-    memset(heap_debug.record, 0, sizeof(struct heap_debug_record) * HEAP_DEBUG_RECORX_MAX_NUM);
+    memset(heap_debug.record, 0, sizeof(struct heap_debug_record) * CONFIG_HEAP_DEBUG_RECORD_MAX_NUM);
     heap_debug.rd_index = 0;
+    heap_debug.rd_index_max = 0;
     k_mutex_unlock(&heap_rd_lock);
 
     return 0;
@@ -343,47 +404,47 @@ static int heap_debug_rd_clear(const struct shell *sh, size_t argc, char **argv)
 SHELL_STATIC_SUBCMD_SET_CREATE(sub_heap_debug_rd,
             SHELL_CMD_ARG(enable, NULL,
                         "heap_debug rd enable",
-                         heap_debug_rd_enable, 0, 0),
+                         cmd_heap_debug_rd_enable, 0, 0),
             SHELL_CMD_ARG(disable, NULL,
                         "heap_debug rd disable\n",
-                         heap_debug_rd_disable, 0, 0),
+                         cmd_heap_debug_rd_disable, 0, 0),
             SHELL_CMD_ARG(dump, NULL,
                         "heap_debug rd dump\n",
-                         heap_debug_rd_dump, 0, 0),
+                         cmd_heap_debug_rd_dump, 0, 0),
             SHELL_CMD_ARG(clear, NULL,
                         "heap_debug rd clear\n",
-                         heap_debug_rd_clear, 0, 0),
+                         cmd_heap_debug_rd_clear, 0, 0),
             SHELL_SUBCMD_SET_END );
+#endif /* CONFIG_HEAP_DEBUG_RD_LSQSH */
 
 SHELL_STATIC_SUBCMD_SET_CREATE(sub_heap_debug_cs,
             SHELL_CMD_ARG(enable, NULL,
                     "Usage:\n"
                     "heap_debug cs enable\n",
-                    heap_debug_cs_enable, 0, 0),
+                    cmd_heap_debug_cs_enable, 0, 0),
             SHELL_CMD_ARG(disable, NULL,
                     "Usage:\n"
                     "heap_debug cs disable\n",
-                    heap_debug_cs_disable, 0, 0),
-
+                    cmd_heap_debug_cs_disable, 0, 0),
             SHELL_CMD_ARG(range, NULL,
-                        "heap_debug cs range <cmp> <size> [<cmp> <size>]\n \
-                         heap_debug cs range close\n",
-                         heap_debug_cs_range, 2, 5),
-
-            SHELL_CMD_ARG(name, NULL,
-                        "heap_debug cs name <thread_name>\n\
-                         heap_debug cs name close\n",
-                         heap_debug_cs_name, 2, 0),
-
+                    "heap_debug cs range <cmp> <size> [<cmp> <size>]\n"
+                    "heap_debug cs range close\n",
+                    cmd_heap_debug_cs_range, 2, 5),
             SHELL_SUBCMD_SET_END );
 
 SHELL_STATIC_SUBCMD_SET_CREATE(sub_heap_debug,
             SHELL_CMD(cs, &sub_heap_debug_cs,
                     "heap_debug cs <range|name> ...",
                     NULL),
+#if defined(CONFIG_HEAP_DEBUG_RD_LSQSH)
             SHELL_CMD(rd, &sub_heap_debug_rd,
                     "heap_debug rd <enable|disable|dump>",
                     NULL),
+#endif /* CONFIG_HEAP_DEBUG_RD_LSQSH */
+            SHELL_CMD_ARG(thread_name, NULL,
+                        "heap_debug thread_name <thread_name>\n"
+                        "heap_debug thread_name close\n",
+                        heap_debug_thread_name, 2, 0),
             SHELL_SUBCMD_SET_END);
 
 SHELL_CMD_REGISTER(heap_debug, &sub_heap_debug,
