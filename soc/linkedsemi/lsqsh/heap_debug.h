0e453ad8d9a895fa08764d319ce22e558be49629
Hunter Liu <htliu@linkedsemi.com>
2026-05-06 23:58:02 -0400

    soc/linkedsemi/lsqsh/heap_debug.h: optimize

--- soc/linkedsemi/lsqsh/heap_debug.h
+++ soc/linkedsemi/lsqsh/heap_debug.h
@@ -1,10 +1,14 @@
 #ifndef __HEAP_DEBUG_H__
 #define __HEAP_DEBUG_H__
 
-void heap_debug_callstack(const char *func, size_t ptr, size_t size);
-void heap_debug_ptr_push(void *ptr, size_t size);
-void heap_debug_ptr_pop(void *ptr);
+#include <zephyr/kernel.h>
+
+void heap_debug_callstack(const char *func, void *ptr, size_t size);
+void heap_debug_ptr_push(const char *alloc_name, void *ptr, size_t size);
+void heap_debug_ptr_pop(void *ptr, size_t size);
+void heap_debug_ptr_replace(void *old_ptr, size_t old_size, const char *alloc_name, void *new_ptr, size_t new_size);
 void heap_debug_ptr_dump(void);
 bool heap_debug_cs_is_enable(void);
+int heap_debug_rd_enable();
 
 #endif //__HEAP_DEBUG_H__
