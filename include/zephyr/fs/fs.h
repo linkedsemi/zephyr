8710623b1dc2b5146e90e5a0686628d45a4c4ac8
wzou <wzou@linkedsemi.com>
2026-05-26 15:26:34 +0800

    feat(overlayfs): add overlayfs type
    Signed-off-by: wzou <wzou@linkedsemi.com>

--- include/zephyr/fs/fs.h
+++ include/zephyr/fs/fs.h
@@ -69,6 +69,9 @@ enum {
 
 	/** Base identifier for external file systems. */
 	FS_TYPE_EXTERNAL_BASE,
+
+    /** Identifier for in-tree OverlayFS file system. */
+	FS_OVERLAYFS,
 };
 
 /** Flag prevents formatting device if requested file system not found */
