68a4a41126998cac8e5a2d3b2a318e19a26c8a24
houruiying <ryhou@linkedsemi.com>
2026-06-04 16:06:28 +0800

    update the sha512 driver

--- drivers/crypto/crypto_linkedsemi_sha512.h
+++ drivers/crypto/crypto_linkedsemi_sha512.h
@@ -26,23 +26,26 @@
 #define SHA512_PADDING_ZERO         (0x00)
 #define SHA512_PADDING_BYTE         (0x80)
 #define SHA512_BLOCK_BYTE_SIZE      (0x80)
-#define SHA512_BLOCK_WORD_SIZE      (SHA512_BLOCK_BYTE_SIZE / sizeof(uint32_t))
-#define SHA512_FIANL_LENGTH         (SHA512_BLOCK_BYTE_SIZE - SHA512_TOTAL_LEN_BYTE)
 
+#ifndef MAX_BLOCK_SIZE
+#define MAX_BLOCK_SIZE 8
+#endif
 __attribute__((aligned(32))) struct sha512_linkedsemi_data {
-	uint32_t buffer[SHA512_BLOCK_WORD_SIZE];
-	struct k_mutex sha512_engine_mutex;
-	struct k_sem calc_end_sem;
-	uint32_t buf_idx;
-	uint32_t total_len;
-	uint8_t result_word_num;
-	uint8_t algo;
-	bool is_first;
+    struct k_mutex sha512_engine_mutex;
+    struct k_sem calc_end_sem;
+    __attribute__((aligned(4))) uint8_t buffer[SHA512_BLOCK_BYTE_SIZE];
+    __attribute__((aligned(4))) uint8_t temp_sram_buffer[MAX_BLOCK_SIZE*SHA512_BLOCK_BYTE_SIZE];
+    uint32_t buffer_idx  ;
+    uint64_t total_length;
+    bool first_update;
+    uint8_t result_word_num;
+    uint8_t algo;
 };
 
 struct sha512_linkedsemi_config {
-	reg_sha512_t *reg; /* SHA512 engine base address */
-	void (*irq_config_func)(const struct device *);
+    reg_sha512_t *reg; /* SHA512 engine base address */
+    void (*irq_config_func)(const struct device *);
+    uint32_t irqn;
     IF_ENABLED(CONFIG_CLOCK_CONTROL, (struct ls_clk_cfg ccfg;))
     IF_ENABLED(CONFIG_RESET, (struct reset_dt_spec reset;))
 };
