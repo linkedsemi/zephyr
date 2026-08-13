5be92b01152fc96c83e2e46f3a2bb595e9e8a01d
Jc Shi <jcshi@linkedsemi.com>
2026-08-13 14:26:28 +0800

    drivers/crypto/crypto_linkedsemi_otbn.c :  Fix the incorrect configuration method of INTR_CTRL _INTR_MSK

--- drivers/crypto/crypto_linkedsemi_otbn.c
+++ drivers/crypto/crypto_linkedsemi_otbn.c
@@ -213,7 +213,7 @@ static int ls_otbn_init(const struct device *dev)
         SYSC_SEC_CPU->INTR_CLR_MSK = FIELD_BUILD(SYSC_SEC_CPU_I_EDN_RND_REQ, 1) |
                               FIELD_BUILD(SYSC_SEC_CPU_I_EDN_URND_REQ, 1) |
                               FIELD_BUILD(SYSC_SEC_CPU_I_OTBN_OTP_REQ, 1);
-    SYSC_SEC_CPU->INTR_CTRL_INTR_MSK = FIELD_BUILD(SYSC_SEC_CPU_I_EDN_RND_REQ, 1) |
+    SYSC_SEC_CPU->INTR_CTRL_INTR_MSK |= FIELD_BUILD(SYSC_SEC_CPU_I_EDN_RND_REQ, 1) |
                               FIELD_BUILD(SYSC_SEC_CPU_I_EDN_URND_REQ, 1) |
                               FIELD_BUILD(SYSC_SEC_CPU_I_OTBN_OTP_REQ, 1);
 
